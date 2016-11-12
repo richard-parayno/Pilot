4397f3f80c37abaa4b4539eef6b43bdce3eceb23
Richard Parayno <broodheart@gmail.com>
2016-11-13 07:48:03 +0800

    scrapped card vaulting for easier checkout process

--- app/src/main/java/com/hackathon/teamprogog/pilot/BillActivity.java
+++ app/src/main/java/com/hackathon/teamprogog/pilot/BillActivity.java
@@ -1,7 +1,10 @@
 package com.hackathon.teamprogog.pilot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.paymaya.sdk.android.PayMayaConfig;
 import com.paymaya.sdk.android.checkout.PayMayaCheckout;
