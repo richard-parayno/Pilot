96d6add8d3f7a4174fb409166f88d647bf00b0bf
Richard Parayno <broodheart@gmail.com>
2016-11-13 04:09:40 +0800

    work on billing starts

--- app/src/main/java/com/hackathon/teamprogog/pilot/AddCardActivity.java
+++ app/src/main/java/com/hackathon/teamprogog/pilot/AddCardActivity.java
@@ -5,6 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.hackathon.teamprogog.pilot.models.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -26,5 +38,19 @@ public class AddCardActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public interface MyApiEndpointInterface {
+        // Request method and URL specified in the annotation
+        // Callback for the parsed response is the last parameter
+
+        @GET("customers/{id}")
+        Call<User> getCustomer(@Path("id") String username);
+
+        @PUT("customers/{id}")
+        Call<List<User>> groupList(@Path("id") int groupId, @Query("sort") String sort);
+
+        @POST("customers")
+        Call<User> createCustomer(@Body User user);
     }
 }
