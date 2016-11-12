04ade3365f7002cdc435519bf1434ea47bd5a820
Richard Parayno <broodheart@gmail.com>
2016-11-12 15:53:50 +0800

    removed login button

--- app/src/main/java/com/hackathon/teamprogog/pilot/RegistrationActivity.java
+++ app/src/main/java/com/hackathon/teamprogog/pilot/RegistrationActivity.java
@@ -20,7 +20,7 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_registration);
+        setContentView(R.layout.activity_registration);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         // initialize parse
@@ -38,15 +38,6 @@ public class RegistrationActivity extends AppCompatActivity {
         });
 
 
-
-        /**FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });**/
 
     }
 
