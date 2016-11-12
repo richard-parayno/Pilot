09e42cb3f06c92953e68942fbecb52cc87b46d93
jafvillamayor00 <janzvillamayor@yahoo.com>
2016-11-13 02:35:43 +0800

    gumagana na yung call button

--- app/src/main/java/com/hackathon/teamprogog/pilot/CallActivity.java
+++ app/src/main/java/com/hackathon/teamprogog/pilot/CallActivity.java
@@ -8,29 +8,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.*;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
 
 
 public class CallActivity extends AppCompatActivity {
-    private Button callButton;
+    private ImageButton imgBtnCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_call);
+        setContentView(R.layout.content_main);
 
-        callButton = (Button) findViewById(R.id.btnCall);
+        imgBtnCall = (ImageButton) findViewById(R.id.callButton);
 
-        callButton.setOnClickListener(new View.OnClickListener() {
+        imgBtnCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //  callButton.
+
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
                 callIntent.setData(Uri.parse("tel:09284994286"));
 
-                if (ActivityCompat.checkSelfPermission(CallActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
-                    return;
+                if (ActivityCompat.checkSelfPermission(CallActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
 
+                    System.out.println("Button Failed");
+
+                    return;
+                }
                 startActivity(callIntent);
 
             }
