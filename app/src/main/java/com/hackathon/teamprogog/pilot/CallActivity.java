78770141a1f627fc4ba99b4a2b1969e232d54974
jafvillamayor00 <janzvillamayor@yahoo.com>
2016-11-12 20:01:59 +0800

    partial ng Call Activity

---
+++ app/src/main/java/com/hackathon/teamprogog/pilot/CallActivity.java
@@ -0,0 +1,34 @@
+package com.hackathon.teamprogog.pilot;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.*;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class CallActivity extends AppCompatActivity {
+    private Button callButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        callButton = (Button) findViewById(R.id.btnCall);
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0377778888"));
+
+                if (ActivityCompat.checkSelfPermission(CallActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED)
+                    startActivity(callIntent);
+
+            }
+        });
+    }
+}
